641ecf7c48998b72b97af5188c27cbb87c2b255f
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-08-23 16:07:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// Function to fetch data from an API and measure time taken
+async function fetchData(apiUrl) {
+  const startTime = performance.now();
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  const endTime = performance.now();
+  const timeTaken = endTime - startTime;
+  return { data, timeTaken };
+}
+
 // Array of API URLs to fetch data from
 const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/1",
@@ -12,4 +22,38 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
 
-// You can write your code here
+// Function to display results in the table
+function displayResults(promiseType, timeTaken, elementId) {
+  const outputElement = document.getElementById(elementId);
+  outputElement.textContent = `${timeTaken.toFixed(2)} ms`;
+}
+
+// Perform API calls using Promise.all
+async function performPromiseAll() {
+  const startTime = performance.now();
+  try {
+    const results = await Promise.all(apiUrls.map(fetchData));
+    const endTime = performance.now();
+    const timeTaken = endTime - startTime;
+    displayResults("Promise.all()", timeTaken, "output-all");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Perform API calls using Promise.any
+async function performPromiseAny() {
+  const startTime = performance.now();
+  try {
+    const results = await Promise.any(apiUrls.map(fetchData));
+    const endTime = performance.now();
+    const timeTaken = endTime - startTime;
+    displayResults("Promise.any()", timeTaken, "output-any");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Call the functions to perform API calls and display results
+performPromiseAll();
+performPromiseAny();
